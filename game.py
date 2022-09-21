73ac5da4cf8d834900c0f72880dfe9ea794ffb17
41R1PAXA <114050546+41R1PAXA@users.noreply.github.com>
2022-09-21 10:16:09 +0300

    Update game.py

--- game.py
+++ game.py
@@ -1,21 +1,22 @@
 import random
+
 turns = ["rock", "paper", "scissors"]
+while(True):
+    human_turn = input("Your turn is  ")
+    computer_turn = (random.choice(turns))
+    print ("Computer turn is " + computer_turn)
 
-human_turn = input("Your turn is  ")
-computer_turn = (random.choice(turns))
-print ("Computer turn is " + computer_turn)
+    print (f'Human: {human_turn} vs. Computer: {computer_turn}')
 
-print (f'Human: {human_turn} vs. Computer: {computer_turn}')
+    if human_turn == computer_turn:
+        print("DRAW!")
 
-if human_turn == computer_turn:
-    print("DRAW!")
+    elif human_turn == "rock" and computer_turn == "paper":
+        print("Computer wins!")
+    elif human_turn == "paper" and computer_turn == "scissors":
+        print("Computer wins!")
+    elif human_turn == "scissors" and computer_turn == "rock":
+        print("Computer wins!")
 
-elif human_turn == "rock" and computer_turn == "paper":
-    print("Computer wins!")
-elif human_turn == "paper" and computer_turn == "scissors":
-    print("Computer wins!")
-elif human_turn == "scissors" and computer_turn == "rock":
-    print("Computer wins!")
-
-else:
-    print ("human wins!")
+    else:
+        print ("human wins!")
