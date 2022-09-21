f82da6cba3c1e64fc26a54e1a0825b0f1bf93e47
41R1PAXA <114050546+41R1PAXA@users.noreply.github.com>
2022-09-21 09:13:10 +0300

    Update game.py

--- game.py
+++ game.py
@@ -1,1 +1,15 @@
-afasfasfas
+human_turn = input("ENTER YOUR TURN!  ")
+computer_turn = "paper"
+
+if human_turn == computer_turn:
+    print("DRAW!")
+
+elif human_turn == "rock" and computer_turn == "paper":
+    print("Computer wins!")
+elif human_turn == "paper" and computer_turn == "scissors":
+    print("Computer wins!")
+elif human_turn == "scissors" and computer_turn == "rock":
+    print("Computer wins!")
+
+else:
+    print ("human wins!")
